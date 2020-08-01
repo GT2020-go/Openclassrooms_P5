6fee1efaffbd11cb5804d9665a9f83c2512a2d15
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-08-01 18:16:35 +0200

    Update class description

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,18 @@ fetch('api/teddies')
     .then(teddies => teddies.map(teddy => {
         const mainContainer = document.getElementById("myData");
         const div = document.createElement("div")
+        div.setAttribute("id", teddy._id)
+        div.setAttribute("class", "name")
         div.innerHTML = 'Name: ' + teddy.name + ' ' + teddy.price + 'EUR'
+
         const img = document.createElement("img")
         img.setAttribute("src", teddy.imageUrl)
+
+        const divDescrip = document.createElement("div")
+        divDescrip.setAttribute("class", "description")
+        divDescrip.innerHTML = teddy.description
+
         mainContainer.appendChild(div);
         mainContainer.appendChild(img);
+        mainContainer.appendChild(divDescrip);
     }));
