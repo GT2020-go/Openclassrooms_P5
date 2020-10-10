7a4df241ebc70761c1575911d5f56f1be94795e2
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-10-10 11:49:23 +0200

    Update cart

---
+++ public/js/cart.js
@@ -0,0 +1,6 @@
+const insertCart = (cart) => {
+    const cartItem = document.createElement("div")
+    cartItem.setAttribute("class", cartItem)
+
+    document.getElementById("img-wrap").appendChild(imageContainer)
+}
