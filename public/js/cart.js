00f462d92d3c482c9247ef52939d69d42c66ab41
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-10-11 10:35:00 +0200

    Start style line items in cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,17 @@
-const insertCart = (cart) => {
-    const cartItem = document.createElement("div")
-    cartItem.setAttribute("class", cartItem)
-
-    document.getElementById("img-wrap").appendChild(imageContainer)
+//async+await >> peut aussi s'ecrire avec .then
+const getTeddiesById = async (id) => {
+    const response = await fetch('api/teddies/' + id)
+    return await response.json();
 }
+
+const buildMyCart = () => {
+    const cart = JSON.parse(localStorage.getItem('cart'))
+    cart.map(async c => {
+        const teddy = await getTeddiesById(c._id)
+        console.log(teddy)
+        console.log(c)
+    })
+}
+
+buildMyCart()
+
