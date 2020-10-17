0902cb9412b3ab31a3801d9e861f489ca41c5c7f
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-10-17 12:14:50 +0200

    Start build cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -10,8 +10,31 @@ const buildMyCart = () => {
         const teddy = await getTeddiesById(c._id)
         console.log(teddy)
         console.log(c)
+
+        insertMainRow()
+        insertImage("img", teddy.imageUrl)
     })
 }
 
 buildMyCart()
 
+
+
+
+//fonction inserer image:
+
+const insertImage = (imgClass, imgSource) => {
+    const imageContainer = document.createElement("img")
+    imageContainer.setAttribute("class", imgClass)
+    imageContainer.setAttribute("src", imgSource)
+    document.getElementById("productList").appendChild(imageContainer)
+}
+
+//Build the cart element by element
+
+const insertMainRow = () => {
+    const rowProduct = document.createElement("div")
+    rowProduct.setAttribute("class", 'row')
+    rowProduct.setAttribute("id", 'item')
+    document.getElementById("productList").appendChild(rowProduct)
+}
