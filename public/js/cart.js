e3b7bdbadb5db3683e31a0f775a07fe0609222ee
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-10-19 18:14:51 +0200

    Add quantity to itme in cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -10,9 +10,7 @@ const buildMyCart = () => {
         const teddy = await getTeddiesById(c._id)
         console.log(teddy)
         console.log(c)
-
-        insertMainRow()
-        insertImage("img", teddy.imageUrl)
+        insertTeddy(teddy.imageUrl, teddy.name, teddy.description, c.quantity, teddy.price)
     })
 }
 
@@ -23,18 +21,83 @@ buildMyCart()
 
 //fonction inserer image:
 
-const insertImage = (imgClass, imgSource) => {
-    const imageContainer = document.createElement("img")
-    imageContainer.setAttribute("class", imgClass)
-    imageContainer.setAttribute("src", imgSource)
-    document.getElementById("productList").appendChild(imageContainer)
-}
+const insertTeddy = (imgSource, name, description, quantity, price) => {
 
-//Build the cart element by element
-
-const insertMainRow = () => {
     const rowProduct = document.createElement("div")
-    rowProduct.setAttribute("class", 'row')
-    rowProduct.setAttribute("id", 'item')
+    rowProduct.setAttribute("class", 'row rowProduct')
+
+    const columnXs = document.createElement("div")
+    columnXs.setAttribute("class", 'col-xs')
+
+    const imageWrapper = document.createElement("div")
+    imageWrapper.setAttribute("class", 'img-wrap')
+
+    const imageContainer = document.createElement("img")
+    imageContainer.setAttribute("class", "img")
+    imageContainer.setAttribute("src", imgSource)
+
+    imageWrapper.appendChild(imageContainer)
+    columnXs.appendChild(imageWrapper)
+    rowProduct.appendChild(columnXs)
     document.getElementById("productList").appendChild(rowProduct)
+
+
+    // insert name + description
+    const columnSm = document.createElement("div")
+    columnSm.setAttribute("class", 'col-sm')
+
+    const row2 = document.createElement("div")
+    row2.setAttribute("class", 'row')
+
+    const insertName = document.createElement("h3")
+    insertName.innerHTML = name
+
+    const insertDescription = document.createElement("p")
+    insertDescription.innerHTML = description
+
+    row2.appendChild(insertName)
+    row2.appendChild(insertDescription)
+    columnSm.appendChild(row2)
+    rowProduct.appendChild(columnSm)
+
+    // incl. qty:
+    const columnXsQuantity = document.createElement("div")
+    columnXsQuantity.setAttribute("class", 'col-xs')
+
+    const itemQuantity = document.createElement("p")
+    itemQuantity.innerHTML = 'Quantity: ' + quantity + ' item '
+
+    columnXsQuantity.appendChild(itemQuantity)
+    rowProduct.appendChild(columnXsQuantity)
+
+    // incl. price:
+    const columnXsPrice = document.createElement("div")
+    columnXsPrice.setAttribute("class", 'col-xs')
+
+    const itemPrice = document.createElement("p")
+    itemPrice.innerHTML = price / 100 + '.00 EUR'
+
+    columnXsPrice.appendChild(itemPrice)
+    rowProduct.appendChild(columnXsPrice)
+
+
+
+
+    //buttons to add/remove products:
+
+    const rowButtons = document.createElement("div")
+    rowButtons.setAttribute("class", 'container productContols')
+
+    const buttonsContainer = document.createElement("div")
+    buttonsContainer.setAttribute("class", 'btn-group')
+
+    const removeButton = document.createElement("a")
+    removeButton.setAttribute("class", "btn btn-light")
+    removeButton.innerHTML = "Remove Article"
+
+
+    buttonsContainer.appendChild(removeButton)
+    rowButtons.appendChild(buttonsContainer)
+    rowProduct.appendChild(rowButtons)
+
 }
